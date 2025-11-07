a7322b80e3397a3e6ac985261c2492fb6f526071
hoang.nguyenmanh <onmontoan@gmail.com>
2025-11-07 11:17:45 +0700

    update like

--- backend/project/Modules/Posts/Repositories/Interfaces/ILikesRepository.cs
+++ backend/project/Modules/Posts/Repositories/Interfaces/ILikesRepository.cs
@@ -5,7 +5,14 @@ namespace project.Modules.Posts.Repositories.Interfaces;
 
 public interface ILikesRepository
 {
+    Task AddAsync(Likes like);
+    void Delete(Likes like);
+    Task SaveChangesAsync();
     Task<IEnumerable<Likes>> GetAllLikesAsync();
     Task<IEnumerable<Likes>> GetLikesByTargetAsync(string targetType, string targetId);
     Task<IEnumerable<Likes>> GetLikesByStudentAsync(string studentId);
+    Task<Likes?> GetLikeAsync(string studentId, string targetType, string targetId);
+     Task<bool> ExistsTargetAsync(string targetType, string targetId);
+    Task<int> CountLikesAsync(string targetType, string targetId);
+    Task LoadStudentUserAsync(Likes like);
 }
